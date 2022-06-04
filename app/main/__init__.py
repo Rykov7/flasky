c3c4b93699e35c5e092dc693f541cef4c5975d00
Rykov7 <rykov7@protonmail.ch>
2022-06-05 01:39:12 +0300

    14a

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
-from app.models import Permission
+from ..models import Permission
 
 
 @main.app_context_processor
 def inject_permissions():
     return dict(Permission=Permission)
+
+
--- app/models.py
+++ app/models.py
@@ -5,9 +5,10 @@ from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask import current_app
 from datetime import datetime
 import hashlib
-from flask import request
+from flask import request, url_for
 from markdown import markdown
 import bleach
+from app.exceptions import ValidationError
 
 
 class Permission:
@@ -88,6 +89,25 @@ class Post(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
+
+    def to_json(self):
+        json_post = {
+            'url': url_for('api.get_post', id=self.id),
+            'body': self.body,
+            'body_html': self.body_html,
+            'timestamp': self.timestamp,
+            'author_url': url_for('api.get_user', id=self.author_id),
+            'comments_url': url_for('api.get_post_comments', id=self.id),
+            'comment_count': self.comments.count()
+        }
+        return json_post
+
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
 
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
@@ -257,6 +277,32 @@ class User(UserMixin, db.Model):
                 db.session.add(user)
                 db.session.commit()
 
+    def generate_auth_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token, max_age=3600)
+        except:
+            return None
+        return User.query.get(data['id'])
+
+    def to_json(self):
+        json_user = {
+            'url': url_for('api.get_user', id=self.id),
+            'username': self.username,
+            'member_since': self.member_since,
+            'last_seen': self.last_seen,
+            'posts_url': url_for('api.get_user_posts', id=self.id),
+            'followed_posts_url': url_for('api.get_user_followed_posts',
+                                          id=self.id),
+            'post_count': self.posts.count()
+        }
+        return json_user
+
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
@@ -266,8 +312,8 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 
-
 login_manager.anonymous_user = AnonymousUser
+
 
 @login_manager.user_loader
 def load_user(user_id):
